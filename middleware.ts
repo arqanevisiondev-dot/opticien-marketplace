244762def897731f703f7d80e575e31934163589
MehdiGuetta <moustafaxda@gmail.com>
2025-11-27 15:21:44 +0100

    Delete middleware completely to fix Vercel Edge Function size

--- middleware.ts
+++
@@ -1,6 +0,0 @@
-// Minimal middleware - authentication is handled in individual routes and APIs
-// This prevents Edge Function bundle size issues on Vercel
-
-export const config = {
-  matcher: [],
-};
